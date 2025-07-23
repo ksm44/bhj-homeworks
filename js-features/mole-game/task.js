78a9e6d9d47843001eaf1bc66198622926e6e866
“Sergey <savershinin23@gmail.com>
2025-07-23 12:25:07 +0300

    Доработка игры "Убей кротов"

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,30 +1,42 @@
-const allHoles = document.querySelectorAll('.hole');
 const deadMole = document.getElementById('dead');
 const lost = document.getElementById('lost');
+let timeoutId = null;
 
 check = () => {
     if(deadMole.textContent < 10){
-        alert('Вы проиграли.');
+        alert('Вы проиграли😟\nВремя истекло');
     } else {
-        alert('Вы победили!!!');
+        alert('Вы победили😊');
     }
-}
+    restart();
+};
 
-setTimeout(check,15000);
+getHole = (index) => document.getElementById(`hole${index}`);
 
-getHole = (index) => document.getElementById(`hole${index}`)
+restart = () => {
+    deadMole.textContent = '0';
+    lost.textContent = '0';
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(check,15000);
+};
+
+timeoutId = setTimeout(check,15000);
 
 for (let i = 1; i <= 9; i++) {
     let hole = getHole(i);
     hole.addEventListener('click', () => {
+        hole.style = 'transform: translateY(15px)';
+        setTimeout(()=>{hole.style = 'transform: translateY(0)';}, 50)
+        
         if(hole.classList.contains('hole_has-mole')){
             ++deadMole.textContent;
         } else {
             ++lost.textContent;
         }
         
-         if(lost.textContent >= 5){
-            alert('Вы проиграли.');
+        if(lost.textContent >= 5){
+            alert('Вы проиграли😟\nПромахнулись 5 раз');
+            restart();
         }
     });
-}
+};
